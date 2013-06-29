d4087ba1f2f4796aeb14fb9249a474918c61757b
Wilson Wan <wanzheng@gmail.com>
2013-06-30 00:07:13 +0800

    add motor module

--- bangle/bangle.ino
+++ bangle/bangle.ino
@@ -4,6 +4,7 @@
 const int COLOR_RED = (1 << 0);
 const int COLOR_GREEN = (1 << 1);
 const int COLOR_BLUE = (1 << 2);
+void led_setup();
 void led_set_color(int color);
 
 /*
@@ -40,29 +41,59 @@ void led_set_color(int color) {
 		blueIntensity = 0;
 	}
 
-        analogWrite(RED_LED_PIN, redIntensity);
-        analogWrite(GREEN_LED_PIN, greenIntensity);
-        analogWrite(BLUE_LED_PIN, blueIntensity);
+	analogWrite(RED_LED_PIN, redIntensity);
+	analogWrite(GREEN_LED_PIN, greenIntensity);
+	analogWrite(BLUE_LED_PIN, blueIntensity);
 }
 
-void setup() {
-	led_setup();
+/*
+ * Header of MOTOR
+ */
+const int MOTOR_PIN = 8;
+void motor_setup();
+void motor_setOnOff(int onOff);
+
+/*
+ * Implement of motor
+ */
+void motor_setup()
+{
+	pinMode(MOTOR_PIN, OUTPUT); 
+}
+
+void motor_setOnOff(int onOff)
+{
+	digitalWrite(MOTOR_PIN, onOff ? HIGH : LOW); // turns the motor On
 }
 
 /*
  * example
  */
+void setup() {
+	led_setup();
+	motor_setup();
+}
+
 void loop() {
+	motor_setOnOff(1);
 	led_set_color(COLOR_RED);
-        delay(1000);
+	delay(1000);
+
+	motor_setOnOff(0);
 	led_set_color(COLOR_GREEN);
-        delay(1000);
+	delay(1000);
+
+	motor_setOnOff(1);
 	led_set_color(COLOR_BLUE);
-        delay(1000);
+	delay(1000);
+
+	motor_setOnOff(0);
 	led_set_color(COLOR_RED | COLOR_GREEN);
-        delay(1000);
+	delay(1000);
+
 	led_set_color(COLOR_GREEN | COLOR_BLUE);
-        delay(1000);
+	delay(1000);
+
 	led_set_color(COLOR_BLUE | COLOR_RED);
-        delay(1000);
+	delay(1000);
 }
